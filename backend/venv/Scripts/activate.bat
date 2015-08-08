232b741812cc7f73769e117255178a536e22ccd2
Diane <dianemichelli1@gmail.com>
2015-08-08 11:13:45 -0300

    Adicionando paths

--- backend/venv/Scripts/activate.bat
+++ backend/venv/Scripts/activate.bat
@@ -1,5 +1,5 @@
 @echo off
-set "VIRTUAL_ENV=C:\Users\Diane\PycharmProjects\DriSite\backend\venv"
+set "VIRTUAL_ENV=C:\Users\Diane\PycharmProjects\tekton\backend\venv"
 
 if defined _OLD_VIRTUAL_PROMPT (
     set "PROMPT=%_OLD_VIRTUAL_PROMPT%"
